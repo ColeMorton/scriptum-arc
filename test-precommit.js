dba35cf3d4c0cc3c661f7aaf67eed37ba238c8eb
Cole Morton <cole.morton@hotmail.com>
2025-10-15 14:51:41 +1000

    test: verify pre-commit hook functionality

---
+++ test-precommit.js
@@ -0,0 +1,4 @@
+// Test file for pre-commit hook
+const unused = "this will trigger a linting warning";
+
+console.log("Testing pre-commit hook");
---
+++ test-precommit.ts
@@ -0,0 +1,4 @@
+// Test file for pre-commit hook
+const unused: string = "this will trigger a linting warning";
+
+console.log("Testing pre-commit hook");
